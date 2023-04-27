6de8c644b177e6e45a6beb27bf10a2d3a76491be
liuxing <liuliuxing12@gmail.com>
2023-04-27 17:45:22 +0800

    初回コミット
    Signed-off-by: liuxing <liuliuxing12@gmail.com>

---
+++ src/main/java/jp/co/reserved/mic/app/ReservedApplication.java
@@ -0,0 +1,13 @@
+package jp.co.reserved.mic.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/reserved/mic/app/ReservedApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.reserved.mic.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
